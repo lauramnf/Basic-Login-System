1452dd39531e2a4738a6f07ce29b95fed5d615dc
Laura Maciel <laura.neves2008@hotmail.com>
2020-12-20 11:37:10 -0300

    Update

--- server.js
+++ server.js
@@ -1,47 +1,66 @@
 // import the packages
 const express = require('express')
 const bcrypt = require('bcrypt')
+const alert = require('alert')
 const app = express()
 
+// users basic alocation
 var users =[]
 
+// recognizes ejs
 app.set('view-engine', 'ejs')
+// app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.urlencoded({ extendend: false }))
 
-// make a get request and render page
-app.get('/', function (req,res) {
+// make a get request and render first page
+app.get('/',  (req,res) => {
   res.render('index.ejs', {name: 'Laura'})
 })
 
-// make a get request and render page
-app.get('/register', function (req, res){
+// make a get request and render regoster page
+app.get('/register',  (req, res) => {
   res.render('register.ejs')
 })
 
-// make a get request and render page
-app.get('/login', function (req, res){
+// make a get request and render login page
+app.get('/login', (req, res) => {
   res.render('login.ejs')
 })
 
-app.post('/login', async function(req, res){
-  try{
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    users.push({
-      first_name: req.body.fname
-      last_name: req.body.last_name
-      email: req.body.email
-      username: req.body.username
-      password: hashedPassword
-    })
-    res.redirect('/login')
-  } catch{
-    res.redirect('/register')
-  }
-  console.log(users)
+// get peoples login information in a secure way
+app.post('/register', async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10) //hash password, so it can be stored in safety
+  users.push({
+    first_name: req.body.fname,
+    last_name: req.body.lname,
+    email: req.body.email,
+    username: req.body.username,
+    password: hashedPassword
+  })
+  res.redirect('/login')
 })
 
 // route HTTP POST request to login page
-app.post('/login', function (req, res){
-
+app.post('/login', async (req, res) => {
+  var user_valido = false
+  for (elem of users){
+    if(elem['username'] === req.body.username){
+      user_valido = true
+      const valid_password = await bcrypt.compare(req.body.password, elem['password']) // check if the passwords match
+      if(!valid_password){
+        alert('Senha incorreta')
+        res.redirect('/login')
+      }
+      else{
+        res.redirect('/')
+      }
+      break;
+    }
+  }
+  if (user_valido === false){
+    alert('Usuário inexistente')
+    res.redirect('/login')
+  }
 })
 
 // start server
